8e3f891ee5045acf964a698c3552301edfa94957
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-10-09 10:05:30 -0700

    Set.js tests all passing with time complexity included

---
+++ sprint-two/src/set.js
@@ -0,0 +1,26 @@
+var Set = function(){
+  var set = Object.create(setPrototype);
+  set._storage = {};
+  return set;
+};
+
+var setPrototype = {};
+
+//Time Complexity: O(1)
+setPrototype.add = function(item){
+  this._storage[item] = item;
+};
+
+//Time Complexity: O(1)
+setPrototype.contains = function(item){
+  return this._storage[item] !== undefined;
+};
+
+//Time Complexity: O(1)
+setPrototype.remove = function(item){
+  delete this._storage[item];
+};
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
